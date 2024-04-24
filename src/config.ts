e2d76b2846031385f07778fabed9f8a0bd2428bb
gvdp <vdputteglenn@gmail.com>
2024-04-24 21:36:09 +0200

    chore: update sheet config

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,34 @@
 // todo: this should be configurable pre template clone, not included here
 
 export const SHEET_IDS: Record<string, { name: string; sheetID: string }> = {
-  ZES: {
-    sheetID: '1rc0th8HpglQgUVXRRoK_BbtseT925JYkchFB-bWrtds',
-    name: '6 Tegen 6',
+  U16M: {
+    sheetID: '1iRk0TpBAFZ2rB5S3uoi_U_4MYADaoFCoT3LLcYq_6nY',
+    name: 'Meisjes U16',
   },
-  TEST: {
-    name: 'TEST',
-    sheetID: '156ZGnMtCQFmDbQHx4KO_ane9IFnu9Cfx1fHJFX98uQ4',
+  U13M: {
+    sheetID: '1NCIcPN7ZX4p8nX9FnvfZuFY6OW8_RQ6eun82Ov7bR2o',
+    name: 'Meisjes U13',
   },
-  ALGEMEEN: {
-    name: 'ALGEMEEN',
-    sheetID: '1GCIsLxqqKZZwy0vQ3NmwTrjclV6DCdKionf_dq9R_Zo',
-  },
-  U6: { name: 'U6 (zat)', sheetID: '1TTWcsZZklHMe5grLhuSIj0JdDe3_gQAp_mNt5sJ1GsU' },
-  U7: { name: 'U7 (zat)', sheetID: '1qUa2cUi0Qjz9UuNw2_jYoTcGq4pUUx1xquZZZxpAmJY' },
-  U8: { name: 'U8 (zat)', sheetID: '1PR_dze_3Z-3N7tdLEauafRzK8I9U5WEhcCFbVPWsDps' },
-  U9: { name: 'U9 (zat)', sheetID: '1IbPyikFcA1zf1aXJoEQBsVkZsoIjxntolkTxXusJdS8' },
-  U10: { name: 'U10 (zon)', sheetID: '1RNZanUPnM8ZGeGR2t_T5dD-Bvor1VTyG1mUnDlvQwXI' },
-  U11: { name: 'U11 (zon)', sheetID: '1DMV-abiYaqmtdy9sEokC9Vp9db2njgNrJ_5asQyYyQc' },
-  U12: { name: 'U12 (zon)', sheetID: '13gYtM515n7KAkLh1geSu5dxq6dGkC4dJ2nLpglJd8po' },
-  U13: { name: 'U13 (zon)', sheetID: '1K3NQl58w1hJjnQvetp--sIfoaJwj-iiLtf-6pVkoh4s' },
-  U15: { name: 'U15 (zat)', sheetID: '1trXANVIfwcPtOnoUVJjRit_TxCbB2H7a6O_SDjUMBHE' },
-  U17: { name: 'U17 (vrij)', sheetID: '1QAHrlWQMbEH8brAR3ZoFZTPWoBXtB0l1wFf4RPqIYc8' },
+  // ZES: {
+  //   sheetID: '1rc0th8HpglQgUVXRRoK_BbtseT925JYkchFB-bWrtds',
+  //   name: '6 Tegen 6',
+  // },
+  // TEST: {
+  //   name: 'TEST',
+  //   sheetID: '156ZGnMtCQFmDbQHx4KO_ane9IFnu9Cfx1fHJFX98uQ4',
+  // },
+  // ALGEMEEN: {
+  //   name: 'ALGEMEEN',
+  //   sheetID: '1GCIsLxqqKZZwy0vQ3NmwTrjclV6DCdKionf_dq9R_Zo',
+  // },
+  // U6: { name: 'U6 (zat)', sheetID: '1TTWcsZZklHMe5grLhuSIj0JdDe3_gQAp_mNt5sJ1GsU' },
+  // U7: { name: 'U7 (zat)', sheetID: '1qUa2cUi0Qjz9UuNw2_jYoTcGq4pUUx1xquZZZxpAmJY' },
+  // U8: { name: 'U8 (zat)', sheetID: '1PR_dze_3Z-3N7tdLEauafRzK8I9U5WEhcCFbVPWsDps' },
+  // U9: { name: 'U9 (zat)', sheetID: '1IbPyikFcA1zf1aXJoEQBsVkZsoIjxntolkTxXusJdS8' },
+  // U10: { name: 'U10 (zon)', sheetID: '1RNZanUPnM8ZGeGR2t_T5dD-Bvor1VTyG1mUnDlvQwXI' },
+  // U11: { name: 'U11 (zon)', sheetID: '1DMV-abiYaqmtdy9sEokC9Vp9db2njgNrJ_5asQyYyQc' },
+  // U12: { name: 'U12 (zon)', sheetID: '13gYtM515n7KAkLh1geSu5dxq6dGkC4dJ2nLpglJd8po' },
+  // U13: { name: 'U13 (zon)', sheetID: '1K3NQl58w1hJjnQvetp--sIfoaJwj-iiLtf-6pVkoh4s' },
+  // U15: { name: 'U15 (zat)', sheetID: '1trXANVIfwcPtOnoUVJjRit_TxCbB2H7a6O_SDjUMBHE' },
+  // U17: { name: 'U17 (vrij)', sheetID: '1QAHrlWQMbEH8brAR3ZoFZTPWoBXtB0l1wFf4RPqIYc8' },
 }
